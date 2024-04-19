167a2a160fca40cb5af3d58a5dc65c7b9bf27fc1
AugustoPoratti <augusto.poratti1@gmail.com>
2024-04-19 20:45:10 -0300

    .

--- proyects_react_LC3/src/App.jsx
+++ proyects_react_LC3/src/App.jsx
@@ -2,9 +2,7 @@
 import './App.css'
 import All_Beers from './components/All_Beers/All_Beers'
 import beers from './components/Beers/Beers'
-import Beers_Available from './components/Beers_Available/Beers_Available'
-import Beers_Styles from './components/Beers_Styles/Beers_Styles'
-import Red_Ipa from './components/Red_Ipa/Red_Ipa'
+
 
 
 
@@ -12,15 +10,10 @@ import Red_Ipa from './components/Red_Ipa/Red_Ipa'
 function App() {
 
   return (
-    <div >
-    <p>Carta Cervezas</p>
-     <All_Beers beers={beers}/>
-     <p>Cervezas disponibles</p>
-     <Beers_Available beers={beers}/>
-     <p>Cervezas Ipa y Red</p>
-     <Red_Ipa beers={beers}/>
-     <p>Estilos de cervezas</p>
-     <Beers_Styles beers={beers}/>
+    <div>
+      <div>
+        <All_Beers beers={beers}/>
+     </div>
     </div>
   )
 }
--- proyects_react_LC3/src/components/All_Beers/All_Beers.jsx
+++ proyects_react_LC3/src/components/All_Beers/All_Beers.jsx
@@ -1,28 +1,35 @@
-
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row'; // Importa el componente Row de Bootstrap
 
+const BeersList = ({ beers }) => {
+  return (
+    <div className="beer">
+      <Row className="justify-content-center"> {/* Centra las filas horizontalmente */}
+        {beers.map((beer) => (
+          <div key={beer.id} className="col-sm-4 mb-3"> {/* Tamaño de columna y margen inferior */}
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title><h3>{beer.beerName}</h3></Card.Title>
+                <Card.Text>
+                  <p>Estilo: {beer.beerStyle}</p>
+                  <h5>Precio: ${beer.price * 1000}</h5>
+                  <p>{beer.available ? "Disponible" : "No Disponible"}</p>
+                </Card.Text>
+                <Button variant="primary">Ver Info</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </Row>
+    </div>
+  )
+}
 
-const AllBeers = ({beers}) => {
-    return (
-        <div className='text-white'>
-            {
-                beers.map((beer, i) => (
-                    <div key={i}>
-                        <h2 >{beer.beerName}</h2>
-                        <h3 >{beer.beerStyle}</h3>
-                        <span>{beer.price * 1000}</span>
-                    </div>
-                ))
-            }
-        </div>
-    );
+BeersList.propTypes = {
+  beers: PropTypes.array
 };
 
-
-AllBeers.propTypes = {
-    beers: PropTypes.array,
-};
-
-
-export default AllBeers;
+export default BeersList;
 
